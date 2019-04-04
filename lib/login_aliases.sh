afdac54142efb8663513565954adcac7dc60df2d
Siraaj Khandkar <siraaj@khandkar.net>
2019-04-04 17:04:46 -0400

    Add dotnet alias

--- lib/login_aliases.sh
+++ lib/login_aliases.sh
@@ -2,5 +2,6 @@ alias   l='ls -lFhv'
 alias  ll='l -a --color=auto'
 alias lll='l -a --color=never'
 alias tree='tree --dirsfirst'
-alias fsi="ledit $HOME/.dotnet/dotnet $HOME/.dotnet/sdk/2.1.503/FSharp/fsi.exe"
+alias dotnet="$HOME/.dotnet/dotnet"
+alias fsi="dotnet $HOME/.dotnet/sdk/2.1.503/FSharp/fsi.exe"
 alias tm='tmux'
