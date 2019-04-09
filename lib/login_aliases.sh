85bfafef9d76ebe24ea09567876210a10fc3c0a4
Siraaj Khandkar <siraaj@khandkar.net>
2019-04-09 10:29:36 -0400

    Add dunst pause/resume aliases

--- lib/login_aliases.sh
+++ lib/login_aliases.sh
@@ -5,3 +5,5 @@ alias tree='tree --dirsfirst'
 alias dotnet="$HOME/.dotnet/dotnet"
 alias fsi="dotnet $HOME/.dotnet/sdk/2.1.503/FSharp/fsi.exe"
 alias tm='tmux'
+alias  dunst_pause='notify-send DUNST_COMMAND_PAUSE'
+alias dunst_resume='notify-send DUNST_COMMAND_RESUME'
