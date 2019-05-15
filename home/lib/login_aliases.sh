641d30ddcd419dfb53fb855064a41ee925a9c9e3
Siraaj Khandkar <siraaj@khandkar.net>
2019-05-15 11:22:03 -0400

    Switch ll from all (a) to almost-all (A)

--- home/lib/login_aliases.sh
+++ home/lib/login_aliases.sh
@@ -1,6 +1,7 @@
 alias   l='ls -lFhv'
-alias  ll='l -a --color=auto'
-alias lll='l -a --color=never'
+alias  la='l -A'
+alias  ll='la --color=auto'
+alias lll='la --color=never'
 alias tree='tree --dirsfirst'
 alias dotnet="$HOME/.dotnet/dotnet"
 alias fsi="dotnet $HOME/.dotnet/sdk/2.1.503/FSharp/fsi.exe"
