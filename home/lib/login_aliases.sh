f4879502a79d1108f870ade3d2f203bdd5de35f8
Siraaj Khandkar <siraaj@khandkar.net>
2019-06-06 23:59:00 -0400

    Update .NET vars and aliases

--- home/lib/login_aliases.sh
+++ home/lib/login_aliases.sh
@@ -3,8 +3,9 @@ alias  la='l -A'
 alias  ll='la --color=auto'
 alias lll='la --color=never'
 alias tree='tree --dirsfirst'
-alias dotnet="$HOME/.dotnet/dotnet"
-alias fsi="dotnet $HOME/.dotnet/sdk/2.1.503/FSharp/fsi.exe"
+alias dotnet='~/.dotnet/dotnet'
+alias fsi='dotnet ~/.dotnet/sdk/2.2.300/FSharp/fsi.exe'
+alias fsc='dotnet ~/.dotnet/sdk/2.2.300/FSharp/fsc.exe'
 alias tm='tmux'
 alias startx='printf "Use a specialized startx-...\n"'
 alias mans='man $(man -k . | sort | fzf -e | awk "{print \$1}")'
--- home/lib/login_variables.sh
+++ home/lib/login_variables.sh
@@ -2,3 +2,7 @@ export PATH=$HOME/bin:/snap/bin:/sbin:/usr/sbin:$PATH
 export EDITOR=vim
 export VISUAL=$EDITOR
 export DIR_GITHUB="${HOME}/Archives/Software/src/repos/remote/github.com"
+
+# .Net Core
+export DOTNET_ROOT=$HOME/.dotnet
+export PATH=$PATH:$HOME/.dotnet
