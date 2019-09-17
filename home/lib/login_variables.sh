6fe002736730aaa336f10d44568bd36ef8f7aaf7
Siraaj Khandkar <siraaj@khandkar.net>
2019-09-17 18:34:54 -0400

    Add mpd_top script
    finds top $N played songs in mpd log

--- home/lib/login_variables.sh
+++ home/lib/login_variables.sh
@@ -3,6 +3,9 @@ export EDITOR=vim
 export VISUAL=$EDITOR
 export DIR_GITHUB="${HOME}/Archives/Software/src/repos/remote/github.com"
 export DIR_NOTES="$HOME/Documents/Notes"
+export DIR_LOG="$HOME/var/log"
+export DIR_LOG_MPD="$DIR_LOG/mpd"
+export FILE_LOG_MPD="$DIR_LOG_MPD/mpd.log"
 
 # .Net Core
 export DOTNET_ROOT=$HOME/.dotnet
