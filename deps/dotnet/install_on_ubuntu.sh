eef8c618a6c05e5f12dcd10ca5ae2b0868f055ba
Siraaj Khandkar <siraaj@khandkar.net>
2020-11-17 15:17:36 -0500

    Mark dotnet pkg as held

--- deps/dotnet/install_on_ubuntu.sh
+++ deps/dotnet/install_on_ubuntu.sh
@@ -2,12 +2,14 @@
 
 set -e
 
-PKG='packages-microsoft-prod.deb'
+PKG_FILE='packages-microsoft-prod.deb'
+PKG_NAME='dotnet-sdk-3.1=3.1.100-1'
 
 mkdir -p data
-wget https://packages.microsoft.com/config/ubuntu/18.04/"$PKG" -O data/"$PKG"
-sudo apt install ./data/"$PKG"
+wget https://packages.microsoft.com/config/ubuntu/18.04/"$PKG_FILE" -O data/"$PKG_FILE"
+sudo apt install ./data/"$PKG_FILE"
 sudo apt update
 sudo apt install apt-transport-https
 sudo apt update
-sudo apt install dotnet-sdk-3.1=3.1.100-1
+sudo apt install "$PKG_NAME"
+sudo apt-mark hold "$PKG_NAME"
